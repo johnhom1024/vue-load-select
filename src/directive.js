54c4a653d7e08f6c07a470c9408e39af06a2b088
anthony <924358746@qq.com>
2021-05-16 12:23:02 +0800

    feat: 加入unbind方法

--- src/directive.js
+++ src/directive.js
@@ -15,17 +15,30 @@ export default () => {
             const SELECTWRAP = el.querySelector(
                 ".el-select-dropdown .el-select-dropdown__wrap"
             );
-            SELECTWRAP.addEventListener("scroll", function() {
-                // this.scrollTop  这里可能因为浏览器缩放存在小数点的情况，导致了滚动到底部时
-                // scrollHeight 减去滚动到底部时的scrollTop ，依然大于clientHeight 导致无法请求更多数据
-                // 这里将scrollTop向上取整 保证滚到底部时，触发调用
-                const CONDITION =
-                    this.scrollHeight - Math.ceil(this.scrollTop) <= this.clientHeight;
-                // this.scrollTop !== 0 当输入时，如果搜索结果很少，以至于没看到滚动条，那么此时的CONDITION计算结果是true，会执行bind.value()，此时不应该执行，否则搜索结果不匹配
-                if (CONDITION && this.scrollTop !== 0) {
-                    binding.value();
-                }
+            SELECTWRAP.addEventListener("scroll", function () {
+                loadMore.call(this, el, binding);
             });
         },
+        unbind(el, binding) {
+            const SELECTWRAP = el.querySelector(
+                ".el-select-dropdown .el-select-dropdown__wrap"
+            );
+            SELECTWRAP.removeEventListener('scroll', function () {
+                loadMore.call(this, el, binding);
+            });
+        }
     });
 };
+
+
+function loadMore(el, binding) {
+    // this.scrollTop  这里可能因为浏览器缩放存在小数点的情况，导致了滚动到底部时
+    // scrollHeight 减去滚动到底部时的scrollTop ，依然大于clientHeight 导致无法请求更多数据
+    // 这里将scrollTop向上取整 保证滚到底部时，触发调用
+    const CONDITION =
+        this.scrollHeight - Math.ceil(this.scrollTop) <= this.clientHeight;
+    // this.scrollTop !== 0 当输入时，如果搜索结果很少，以至于没看到滚动条，那么此时的CONDITION计算结果是true，会执行bind.value()，此时不应该执行，否则搜索结果不匹配
+    if (CONDITION && this.scrollTop !== 0) {
+        binding.value();
+    }
+}
